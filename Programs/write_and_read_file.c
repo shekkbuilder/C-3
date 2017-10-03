f6e5b83b9b68fd38bc7ba99cccb2b714d6d6d177
jrliv <jrliv@outlook.com>
2017-10-03 12:48:04 -0400

    Add write_and_read_file.c

---
+++ Programs/write_and_read_file.c
@@ -0,0 +1,29 @@
+// Example program that writes to a file then reads and prints its data.
+#include <stdio.h>
+
+main()
+{
+    char input;
+    FILE *file_pointer; // create pointer to file datatype
+
+    printf("Enter information: \n");
+    file_pointer = fopen("datafile.txt", "w");  // open file and write
+
+    while ((input = getchar()) != EOF)  // write characters to file until EOF (EOF = Ctrl+d)
+    {
+        putc(input, file_pointer);        
+    }
+    fclose(file_pointer);   // close the file
+
+    printf("\n");   // print new line to help with readability
+    printf("Here is the information you entered: \n");
+    printf("\n");
+
+    file_pointer = fopen("datafile.txt", "r");  // open file and read
+
+    while ((input = getc(file_pointer)) != EOF)  // read characters from file until EOF
+    {
+        printf("%c", input);    // print characters from file
+    }
+    fclose(file_pointer);
+}
