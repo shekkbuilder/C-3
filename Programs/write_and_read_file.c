6c4ac2758e6bd4e7efd86f3d91e1db30f554af92
jrliv <jrliv@outlook.com>
2017-10-06 16:55:08 -0400

    Edit write_and_read_file.c

--- Programs/write_and_read_file.c
+++ Programs/write_and_read_file.c
@@ -6,7 +6,7 @@ main()
     char input;
     FILE *file_pointer; // create pointer to file datatype
 
-    printf("Enter information: \n");
+    printf("Enter information: \n\n");
     file_pointer = fopen("datafile.txt", "w");  // open file and write
 
     while ((input = getchar()) != EOF)  // write characters to file until EOF (EOF = Ctrl+d)
@@ -15,10 +15,7 @@ main()
     }
     fclose(file_pointer);   // close the file
 
-    printf("\n");   // print new line to help with readability
-    printf("Here is the information you entered: \n");
-    printf("\n");
-
+    printf("Here is the information you entered: \n\n");
     file_pointer = fopen("datafile.txt", "r");  // open file and read
 
     while ((input = getc(file_pointer)) != EOF)  // read characters from file until EOF
